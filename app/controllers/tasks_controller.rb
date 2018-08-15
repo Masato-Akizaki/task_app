00406603c01da155bc3195dad7578b2390ef103f
Masato AKIZAKI <m.graceful_9411@icloud.com>
2018-08-16 06:25:23 +0900

    Created Task Page (add flash)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,7 +11,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      flash[:success] = "タスクを作成しました"
+      flash[:success] = "タスクを登録しました"
       redirect_to root_url
     else
       render 'new'
