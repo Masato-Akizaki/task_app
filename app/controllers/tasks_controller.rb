9a0a27219f4b8dbc8578d6ff47ea1790efbd7533
Masato AKIZAKI <m.graceful_9411@icloud.com>
2018-08-15 15:43:12 +0900

    [WPI] Creating Task Pages

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = task.all
+    @tasks = Task.all
   end
 
   def new
@@ -8,6 +8,15 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.new(
+      name: params[:name],
+      detail: params[:detail]
+    )
+    if @task.save
+      redirect_to("/")
+    else
+      render("tasks/new")
+    end
   end
 
   def show
@@ -15,11 +24,23 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find_by(id: params[:id])
   end
 
   def update
+    @task = Task.find_by(id: params[:id])
+    @task.name = params[:name]
+    @task.detail = params[:detail]
+    if @task.save
+      redirect_to("/")
+    else
+      render("tasks/edit")
+    end
   end
 
   def destroy
+    @task = Task.find_by(id: params[:id])
+    @task.destroy
+    redirect_to("/")
   end
 end
