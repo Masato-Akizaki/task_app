1a4b6352ecaf32e42a9e1b9a037e60fe37d6690f
Masato AKIZAKI <m.graceful_9411@icloud.com>
2018-08-27 14:03:44 +0900

    add deadline & sort

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -73,11 +73,44 @@ RSpec.feature "Tasks", type: :feature do
   end
 
   describe "タスク一覧の表示順" do
-    it "created_at降順で並び替える" do
-      Task.create(id: 1, name: "task1", created_at: Time.current + 1.hours)
-      Task.create(id: 2, name: "task2", created_at: Time.current + 2.hours)
+    before do
+      Task.create(id: 1, name: "task1", created_at: Time.current + 1.hours, deadline: Time.current + 1.month)
+      Task.create(id: 2, name: "task2", created_at: Time.current + 2.hours, deadline: Time.current + 3.month)
+      Task.create(id: 3, name: "task3", created_at: Time.current + 3.hours, deadline: Time.current + 2.month)
+      Task.create(id: 4, name: "task4", created_at: Time.current + 4.hours, deadline: Time.current + 4.month)
+      visit root_path
+    end
 
-      expect(Task.order("created_at DESC").map(&:id)).to eq [2,1]
+    it 'デフォルトがcreated_at降順で表示' do
+      within '.tasks' do
+        task_ids = all('.task-id').map(&:text)
+        expect(task_ids).to eq %w(No.4 No.3 No.2 No.1)
+      end
+    end
+
+    it "created_at昇順で並び替え" do
+      click_link "作成順"
+      within '.tasks' do
+        task_ids = all('.task-id').map(&:text)
+        expect(task_ids).to eq %w(No.1 No.2 No.3 No.4)
+      end
+    end
+
+    it "deadline昇順で並び替え" do
+      click_link "期限順"
+      within '.tasks' do
+        task_ids = all('.task-id').map(&:text)
+        expect(task_ids).to eq %w(No.1 No.3 No.2 No.4)
+      end
+    end
+
+    it "deadline降順で並び替え" do
+      click_link "期限順"
+      click_link "期限順"
+      within '.tasks' do
+        task_ids = all('.task-id').map(&:text)
+        expect(task_ids).to eq %w(No.4 No.2 No.3 No.1)
+      end
     end
   end
 end
