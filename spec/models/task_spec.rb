7af9e2ec45099c731708356c7d73f66ee11d5144
Masato AKIZAKI <m.graceful_9411@icloud.com>
2018-08-31 16:50:09 +0900

    edit RSpec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Task, type: :model do
 
   describe "search task" do
     before do
+      User.create!(id: 1, name: 'user1', email: 'user1@example.com')
       @task1 = Task.create(id: 1, name: "task1", status: "完了", user_id: 1)
       @task2 = Task.create(id: 2, name: "task2", status: "未着手", user_id: 1)
       @task3 = Task.create(id: 3, name: "work3", status: "未着手", user_id: 1)
