1384b3369a22b9094a96b63da8df6c07deeac78c
Masato AKIZAKI <m.graceful_9411@icloud.com>
2018-09-07 18:30:57 +0900

    login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,20 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:index, :show, :edit, :update, :destroy]
+
   def new
     @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "ユーザー登録が完了しました！"
+      redirect_to root_path
+    else
+      redirect_to "new"
+    end
   end
 
   def index
@@ -28,5 +39,4 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                   :password_confirmation)
     end
-
 end
