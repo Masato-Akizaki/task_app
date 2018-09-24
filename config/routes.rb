39e4ee014355e60dbd722b2f5a6d60127e16099d
Masato AKIZAKI <m.graceful_9411@icloud.com>
2018-09-24 15:07:58 +0900

    set_error_page

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,20 @@ Rails.application.routes.draw do
   get    "/login"  => "sessions#new"
   post   "/login"  => "sessions#create"
   delete "/logout" => "sessions#destroy"
+  post   "/tasks/new" => "tasks#create"
+  patch  "/tasks/:id/edit" => "tasks#update"
+  post   "/labels/new" => "labels#create"
+  patch  "/labels/:id/edit" => "labels#update"
+  post   "/admin/users/new" => "admin/users#create"
+  patch  "/admin/users/:id/edit" => "admin/users#update"
 
-  resources :users, except: :index 
-  resources :tasks, except: :index
-  resources :labels
+
+  resources :users, except: [:index, :new, :create, :update] 
+  resources :tasks, except: [:index, :create, :update] 
+  resources :labels, except: [:create, :update] 
   namespace :admin do
-    resources :users
+    resources :users, except: [:create, :update] 
   end
+  
+  get '*path', controller: 'application', action: 'render_404'
 end
