2dd83decaa42a874d26d4b3fa9c383c53bee232a
Masato AKIZAKI <m.graceful_9411@icloud.com>
2018-09-25 11:14:34 +0900

    restore rspec

--- spec/routing/tasks_routing_spec.rb
+++ spec/routing/tasks_routing_spec.rb
@@ -20,12 +20,12 @@ RSpec.describe TasksController, type: :routing do
 
 
     it "routes to #create" do
-      expect(:post => "/tasks/new").to route_to("tasks#create")
+      expect(:post => "/tasks").to route_to("tasks#create")
     end
     
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/tasks/1/edit").to route_to("tasks#update", :id => "1")
+      expect(:patch => "/tasks/1").to route_to("tasks#update", :id => "1")
     end
 
     it "routes to #destroy" do
