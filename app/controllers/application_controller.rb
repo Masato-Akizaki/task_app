34d18485329325d01fb6b3314c60f2c80eac77d5
Masato AKIZAKI <m.graceful_9411@icloud.com>
2018-09-25 11:10:57 +0900

    restore routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   # 例外処理
-#rescue_from Exception, with: :render_500
-#rescue_from ActiveRecord::RecordNotFound, with: :render_404
-#rescue_from ActionController::RoutingError, with: :render_404
-#rescue_from NoMethodError, with: :render_404
+rescue_from Exception, with: :render_500
+rescue_from ActiveRecord::RecordNotFound, with: :render_404
+rescue_from ActionController::RoutingError, with: :render_404
+rescue_from NoMethodError, with: :render_404
 
 def render_403
   render template: 'errors/error_403', status: 403, layout: 'application', content_type: 'text/html'
--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,13 @@ Rails.application.routes.draw do
   get    "/login"  => "sessions#new"
   post   "/login"  => "sessions#create"
   delete "/logout" => "sessions#destroy"
-  post   "/tasks/new" => "tasks#create"
-  patch  "/tasks/:id/edit" => "tasks#update"
-  post   "/labels/new" => "labels#create"
-  patch  "/labels/:id/edit" => "labels#update"
-  post   "/admin/users/new" => "admin/users#create"
-  patch  "/admin/users/:id/edit" => "admin/users#update"
 
 
-  resources :users, except: [:index, :new, :create, :update] 
-  resources :tasks, except: [:index, :create, :update] 
-  resources :labels, except: [:create, :update] 
+  resources :users, except: [:index, :new, :create] 
+  resources :tasks, except: [:index] 
+  resources :labels
   namespace :admin do
-    resources :users, except: [:create, :update] 
+    resources :users 
   end
   
   get '*path', controller: 'application', action: 'render_404'
